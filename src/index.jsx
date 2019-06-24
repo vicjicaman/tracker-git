88cfcd975dea2563ddcb0d3f8bf0062e1da294d1
Victor Jimenez <vic.jicama@gmail.com>
2019-06-24 18:07:52 +0200

    Separate api ready packages The packages that are stable and used in more than one project are git, events, operations, ping and stream.

---
+++ src/index.jsx
@@ -0,0 +1,21 @@
+import {clone} from './clone'
+import * as Files from './files'
+import * as Branches from './branches'
+import * as Commits from './commits'
+import * as Sync from './sync'
+import * as Query from './query'
+import * as Merges from './merges'
+import * as Stash from './stashes'
+import * as Exec from './exec'
+
+export {
+  clone,
+  Files,
+  Branches,
+  Commits,
+  Sync,
+  Query,
+  Merges,
+  Stash,
+  Exec
+}
